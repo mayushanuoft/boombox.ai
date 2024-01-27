9eaf0892e345444dda1f5edc30dd8442da89fbbd
Sponge <kalebt567@gmail.com>
2024-01-27 02:46:40 -0500

    background final

--- frontEnd/vite-project/src/App.jsx
+++ frontEnd/vite-project/src/App.jsx
@@ -1,16 +1,26 @@
-// import { useState } from 'react'
-import './App.css'
-import { BoomBox } from './components/boombox'
+import "./App.scss";
+import { BoomBox } from "./components/boombox";
+
+//Imports
+import video from "./assets/synthCityBg.mp4";
 
 function App() {
 
   return (
-    <h1>
-      Hello
-      <BoomBox />
-    </h1>
+    <div className="App">
+      <video
+        className="fullscreen-video"
+        autoPlay
+        muted
+        loop
+        preload="auto"
+      >
+        <source src={video} type="video/mp4" />
+      </video>
 
-  )
+      <BoomBox />
+    </div>
+  );
 }
 
-export default App
+export default App;
