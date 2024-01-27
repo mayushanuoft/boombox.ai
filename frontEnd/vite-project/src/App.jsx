fef86c94d8c056ad85e818230ed09ccfbd38efbc
Sidak <magicalsidak@gmail.com>
2024-01-27 00:26:55 -0500

    assests folder

--- frontEnd/vite-project/src/App.jsx
+++ frontEnd/vite-project/src/App.jsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
+import { BoomBox } from './components/boombox'
 
 function App() {
 
   return (
     <h1>
       Hello
+      <BoomBox />
     </h1>
 
   )
