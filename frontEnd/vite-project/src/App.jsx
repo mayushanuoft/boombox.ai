6117ea4ecc9fcb740a81b0e5d8804a52fffaad54
Sponge <kalebt567@gmail.com>
2024-01-27 18:25:21 -0500

    facial capture ready

--- frontEnd/vite-project/src/App.jsx
+++ frontEnd/vite-project/src/App.jsx
@@ -1,24 +1,26 @@
-import "./App.scss";
-import { BoomBox } from "./components/boombox";
-
-//Imports
+import { useState, useEffect } from "react";
 import video from "./assets/synthCityBg.mp4";
+import { BoomBox } from "./components/boombox.jsx";
+import FacialRecModal from "./components/FacialRec/FacialRecModal";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsOpen(true);
+      console.log(isOpen)
+    }, 1000);
+  }, []);
 
   return (
     <div className="App">
-      <video
-        className="fullscreen-video"
-        autoPlay
-        muted
-        loop
-        preload="auto"
-      >
+      <video className="fullscreen-video" autoPlay muted loop preload="auto">
         <source src={video} type="video/mp4" />
       </video>
 
       <BoomBox />
+      <FacialRecModal open={isOpen} />
     </div>
   );
 }
